bf20e0b03060436cf9a82bedccbcda93c18fc17b
Deb Koch <92552721+DebraMK@users.noreply.github.com>
2022-08-21 19:49:43 -0500

    password hashed

--- index.js
+++ index.js
@@ -7,26 +7,25 @@ const readlineSync = require('readline-sync')
 // We'll keep a global object to store usernames and password hashes
 let globalStore = {}
 
-
-
 /*
 * SOLUTION CODE FOR BCRYPT FUNCTIONS
 */
 
 // function for checking a password
 checkPassword = async (username, plaintextPassword) => {
-    // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
     // Ensure global store contains the user 
     // (this is a quick way to check if an object contains a key)
     if (globalStore[username]) {
+        let result = await bcrypt.compare(plaintextPassword, globalStore[username])
         // TODO: Use bcrypt's compare methof to compare a plaintext password to a password hash
-
         // TODO: The result variable is a boolean. True means the user was valid. Take action accordingly.
         if (result) {
+            console.log(`Welcome, ${username}`)
             // TODO: Display message for valid credentials
+            // return {Welcome ${username}}
         }
         else {
+            console.log(`invalid entry`)
             // TODO: Display message for invalid credentials
         }
     }
@@ -37,14 +36,14 @@ checkPassword = async (username, plaintextPassword) => {
 }
 
 hashPassword = async (username, password) => {
-    // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
+    let hashedPassword = await bcrypt.hash(password, 12)
 
     // TODO: Make the password hash using bcrypt
 
     // TODO: Add the user and password hash to the global store object
-
+globalStore[username] = hashedPassword
     // TODO: Print a status update including the username and password hash
+    console.log(`user ${username} was added.  Their password hash is ${hashedPassword}`)
 }
 
 
